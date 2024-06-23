46ff1a174b11030262ef6928e2934fa2a48d1061
David Doberstein <david.doberstein@gmail.com>
2024-06-23 00:41:58 -0700

    wrote code for optionBlurt.py

--- optionBlurt.py
+++ optionBlurt.py
@@ -1,1 +1,7 @@
 from functions import get_answer_from_chatgpt, extract_text_from_pdf
+
+def unknown_info_from_blurt(userInput, stringOfPDF):
+    question =  + "Compare the userblurt with the PDF text to assess conceptual alignment and identify topics excluded from the user's blurt."
+    context = "Call this text userInput:\n" + userInput + "\nCall this text PDF:\n" + stringOfPDF
+    return get_answer_from_chatgpt( question, context)
+    
